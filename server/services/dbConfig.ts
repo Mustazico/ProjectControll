57b6e5cc62dc781f1f9f5acc5e6338525d639c06
Eirik Brakstad <eirik.brakstad@hotmail.com>
2025-05-04 16:55:24 +0200

    Config

--- server/services/dbConfig.ts
+++ server/services/dbConfig.ts
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
 
 const dbConfig = {
   server: process.env.DB_SERVER,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: Number(process.env.DB_PORT)
   // Add other necessary configuration options
 };
 
